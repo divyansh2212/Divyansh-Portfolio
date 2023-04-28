a0cbaf47de246e14e6e0edd445b49b4a4d95b221
Divyansh Mittal <mittaldivyansh2212@gmail.com>
2023-04-28 21:10:31 +0530

    Added achievements section

--- src/Components/Achievements/Achievements.js
+++ src/Components/Achievements/Achievements.js
@@ -1,16 +1,15 @@
 import React from "react";
 import styles from "./Achievements.module.css";
+import { data } from "./data.js";
 
 const Achievements = () => {
   return (
     <div id="achievements">
       <h1 className={styles.heading}>Achievements</h1>
-      <ul>
-        <li>abcde</li>
-        <li>abcde</li>
-        <li>abcde</li>
-        <li>abcde</li>
-        <li>abcde</li>
+      <ul className={styles.point_list}>
+        {data.map((point) => {
+          return <li>{point}</li>;
+        })}
       </ul>
     </div>
   );
